d939932ea5fc926eb3a2db03f8021368ef22c4ff
EnglishAlphabet <ggs5times@gmail.com>
2024-11-21 11:33:16 +0630

    Add files via upload

--- src/main/java/Model/Assignment_Submissions.java
+++ src/main/java/Model/Assignment_Submissions.java
@@ -1,5 +1,57 @@
 package Model;
 
-public class Assignment_Submissions {
+import java.time.LocalDateTime;
 
+public class Assignment_Submissions {
+	private int submission;
+	private Assignments assignment;
+	private Users user;
+	private String assignment_file;
+	private LocalDateTime submitted_at;
+	
+	public Assignment_Submissions() {
+	}
+
+	public int getSubmission() {
+		return submission;
+	}
+
+	public void setSubmission(int submission) {
+		this.submission = submission;
+	}
+
+	public Assignments getAssignment() {
+		return assignment;
+	}
+
+	public void setAssignment(Assignments assignment) {
+		this.assignment = assignment;
+	}
+
+	public Users getUser() {
+		return user;
+	}
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+
+	public String getAssignment_file() {
+		return assignment_file;
+	}
+
+	public void setAssignment_file(String assignment_file) {
+		this.assignment_file = assignment_file;
+	}
+
+	public LocalDateTime getSubmitted_at() {
+		return submitted_at;
+	}
+
+	public void setSubmitted_at(LocalDateTime submitted_at) {
+		this.submitted_at = submitted_at;
+	}
+	
+	
+	
 }
