5104985019a458da118dc0a8edac3527c3491f78
XERAN <htoosetpaing1123@gmail.com>
2024-11-23 17:55:47 +0630

    ver1.0, main

--- src/main/java/com/ss/studysystem/Model/Student_Answers.java
+++ src/main/java/com/ss/studysystem/Model/Student_Answers.java
@@ -7,50 +7,64 @@ public class Student_Answers {
 	private Test test;
 	private Users user;
 	private Questions question;
-	private int answer;
+	private Answers answer;
 	private Boolean is_correct;
 	private LocalDateTime attempted_at;
+
 	public Student_Answers() {
 	}
 	public int getResponse() {
 		return response;
 	}
+
 	public void setResponse(int response) {
 		this.response = response;
 	}
+
 	public Test getTest() {
 		return test;
 	}
+
 	public void setTest(Test test) {
 		this.test = test;
 	}
+
 	public Users getUser() {
 		return user;
 	}
+
 	public void setUser(Users user) {
 		this.user = user;
 	}
+
 	public Questions getQuestion() {
 		return question;
 	}
+
 	public void setQuestion(Questions question) {
 		this.question = question;
 	}
-	public int getAnswer() {
+
+	public Answers getAnswer() {
 		return answer;
 	}
-	public void setAnswer(int answer) {
+
+	public void setAnswer(Answers answer) {
 		this.answer = answer;
 	}
+
 	public Boolean getIs_correct() {
 		return is_correct;
 	}
+
 	public void setIs_correct(Boolean is_correct) {
 		this.is_correct = is_correct;
 	}
+
 	public LocalDateTime getAttempted_at() {
 		return attempted_at;
 	}
+
 	public void setAttempted_at(LocalDateTime attempted_at) {
 		this.attempted_at = attempted_at;
 	}
