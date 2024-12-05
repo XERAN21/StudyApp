8fe7fd61955bcba3d7782f4bddee8cfc94c20ea7
NKS <ggs5times@gmail.com>
2024-12-05 15:51:09 +0630

    ver1.0, main

--- src/main/java/com/ss/studysystem/controller/to_do_list_view.java
+++ src/main/java/com/ss/studysystem/controller/to_do_list_view.java
@@ -1,6 +1,9 @@
 package com.ss.studysystem.controller;
 
 import com.ss.studysystem.Model.Frequency;
+import com.ss.studysystem.Model.To_Do_List;
+import com.ss.studysystem.Model.Users;
+import com.ss.studysystem.database.controller.to_do_list_controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,6 +15,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.time.LocalDate;
 
 public class to_do_list_view {
 
@@ -51,6 +55,24 @@ public class to_do_list_view {
 
             //Database controller logic
 
+            int uid = 1; //todo change this later
+            String today = LocalDate.now().getDayOfWeek().toString().substring(0,3).toUpperCase();
+            Frequency freq = to_do_list.getCurrent_freq();
+            System.out.println(freq);
+
+            Users users = new Users();
+            users.setId(uid);
+
+            To_Do_List toDoList = new To_Do_List();
+            toDoList.setUser(users);
+            toDoList.setContent(new_task_field.getText());
+            toDoList.setFreq(freq);
+            toDoList.setIs_complete(false);
+ int[] inserted_id = new int[1];
+            if(!to_do_list_controller.create_to_do_list(toDoList, inserted_id)) return; //todo throws error
+
+            toDoList.setTo_do_list(inserted_id[0]);
+
             URL item_path = getClass().getResource("/com/ss/studysystem/Fxml/to_do_list_item.fxml");
             FXMLLoader item_loader = new FXMLLoader(item_path);
             Node item = item_loader.load();
@@ -58,6 +80,7 @@ public class to_do_list_view {
             to_do_list_item tdl_ctrl = item_loader.getController();
             tdl_ctrl.set_task(new_task_field.getText());
             tdl_ctrl.set_parent(list_view);
+            tdl_ctrl.setToDoList(toDoList);
 
             list_view.getChildren().add(item);
 
