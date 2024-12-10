5cfd2fd8af5e3a61ef6a1221fa90d05e0521de27
NKS <ggs5times@gmail.com>
2024-12-10 14:21:17 +0630

    ver1.0, main

---
+++ src/main/java/com/ss/studysystem/database/controller/question_controller.java
@@ -0,0 +1,112 @@
+package com.ss.studysystem.database.controller;
+
+import com.ss.studysystem.Model.Questions;
+import com.ss.studysystem.Model.Users;
+import com.ss.studysystem.database.connection.DB_Connection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class question_controller {
+
+    public static boolean create_question(Questions questions) {
+        String sql = "CALL CreateQuestion(?,?,?)";
+        try (Connection connection = DB_Connection.Get_Connection();
+             CallableStatement callableStatement = connection.prepareCall(sql)) {
+
+            callableStatement.setInt(1, questions.getId());
+            callableStatement.setString(2, questions.getQuestion_text());
+            callableStatement.setString(3, String.valueOf(questions.getQuestion_img()));
+
+            int row_affected = callableStatement.executeUpdate();
+            return row_affected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static Questions get_question(int id) {
+        String sql = "CALL GetQuestion(?)";
+        Questions questions = new Questions();
+        try (Connection con = DB_Connection.Get_Connection();
+             CallableStatement callableStatement = con.prepareCall(sql)) {
+
+            callableStatement.setInt(1, id);
+
+            ResultSet resultSet = callableStatement.executeQuery();
+
+            while (resultSet.next()) {
+                questions.setId(resultSet.getInt("question_id"));
+                questions.setQuestion_text(resultSet.getString("question_text"));
+                questions.setQuestion_img(resultSet.getBlob("question_img"));
+            }
+            return questions;
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static List<Questions> get_all_questions(int id) {
+        List<Questions> Questions_list = new ArrayList<>();
+        String sql = "{CALL Get_all_question(?)}";
+
+        try (Connection con = DB_Connection.Get_Connection();
+             CallableStatement callableStatement = con.prepareCall(sql)) {
+
+            callableStatement.setInt(1, id);
+            ResultSet resultSet = callableStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Questions questions = new Questions();
+
+                questions.setId(resultSet.getInt("question_id"));
+                questions.setQuestion_text(resultSet.getString("question_text"));
+                questions.setQuestion_img(resultSet.getBlob("question_img"));
+                Questions_list.add(questions);
+            }
+            return Questions_list;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public boolean update_question(Questions questions) {
+        String sql = "CALL update_question(?,?,?)";
+        try (Connection connection = DB_Connection.Get_Connection();
+             CallableStatement callableStatement = connection.prepareCall(sql)) {
+
+            callableStatement.setInt(1, questions.getId());
+            callableStatement.setString(2, questions.getQuestion_text());
+            callableStatement.setBlob(3, questions.getQuestion_img());
+
+            int row_affected = callableStatement.executeUpdate();
+            return row_affected > 0;
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean delete_question(int id){
+        String sql = "CALL delete_question(?)";
+        try(Connection connection = DB_Connection.Get_Connection();
+            CallableStatement callableStatement = connection.prepareCall(sql)){
+            callableStatement.setInt(1,id);
+
+            int row_affected = callableStatement.executeUpdate();
+            return row_affected > 0;
+        } catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
+
