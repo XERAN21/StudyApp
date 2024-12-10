4415cf45c114a5b3a023f5dfcfac5ffac0c37fdb
NKS <ggs5times@gmail.com>
2024-12-10 09:24:49 +0630

    ver1.0, main

--- src/main/java/com/ss/studysystem/Model/Questions.java
+++ src/main/java/com/ss/studysystem/Model/Questions.java
@@ -1,23 +1,37 @@
 package com.ss.studysystem.Model;
 
+import java.sql.Blob;
+
 public class Questions {
 	private int id;
 	private String question_text;
+	private Blob question_img;
 
 	public Questions() {
 
 	}
-	public int getQuestion() {
+
+	public int getId() {
 		return id;
 	}
-	public void setQuestion(int question) {
-		this.id = question;
+
+	public void setId(int id) {
+		this.id = id;
 	}
+
 	public String getQuestion_text() {
 		return question_text;
 	}
+
 	public void setQuestion_text(String question_text) {
 		this.question_text = question_text;
 	}
-	
+
+	public Blob getQuestion_img() {
+		return question_img;
+	}
+
+	public void setQuestion_img(Blob question_img) {
+		this.question_img = question_img;
+	}
 }
