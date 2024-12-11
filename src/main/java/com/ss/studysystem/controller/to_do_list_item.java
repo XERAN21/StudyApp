b3109e309c6b50737ae847be817114d1bc6deb4e
nomar <thantzin15599@gmail.com>
2024-12-11 10:49:24 +0630

    ver1.0, main

--- src/main/java/com/ss/studysystem/controller/to_do_list_item.java
+++ src/main/java/com/ss/studysystem/controller/to_do_list_item.java
@@ -1,14 +1,17 @@
 package com.ss.studysystem.controller;
 
 import com.ss.studysystem.Model.To_Do_List;
+import com.ss.studysystem.UI.components.modal_builder;
 import com.ss.studysystem.database.controller.to_do_list_controller;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class to_do_list_item {
 
@@ -36,7 +39,7 @@ public class to_do_list_item {
         delete.setOnAction(e -> delete_task());
         check.setOnAction(e -> complete_task());
 
-        task_col.widthProperty().addListener((ob,ov,nv)->{
+        task_col.widthProperty().addListener((ob, ov, nv) -> {
             double w = nv.doubleValue();
             task.setWrappingWidth(w);
         });
@@ -52,8 +55,38 @@ public class to_do_list_item {
         //todo ask for user confirmation
 
         //todo Database controller logic
-        System.out.println(toDoList.getTo_do_list());
-        if(!to_do_list_controller.delete_to_do_list(toDoList.getTo_do_list())) return; //todo throws error msg
+        try {
+            FXMLLoader alert_mdl = new FXMLLoader(getClass().getResource("/com/ss/studysystem/Fxml/modals/del_task_confirmation_type_a.fxml"));
+            Parent alert_view = alert_mdl.load();
+            Stage stage = modal_builder.build_fixed_modal((Stage) delete.getScene().getWindow(), alert_view, 447, 248);
+            del_task_controller del_ctrl = alert_mdl.getController();
+
+            del_ctrl.setToDoList(toDoList);
+
+            del_ctrl.setOnResult(result -> {
+                if (result) {
+                    boolean success = to_do_list_controller.delete_to_do_list(toDoList.getTo_do_list());
+                    if (success) {
+                        remove_task();
+                        System.out.println("Task deleted successfully!");
+                    } else {
+                        System.out.println("Failed to delete task.");
+                    }
+                } else {
+                    System.out.println("Delete task operation was cancelled.");
+                }
+            });
+
+            stage.showAndWait();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public void remove_task() {
         parent.getChildren().remove(task_root);
     }
 
@@ -68,7 +101,7 @@ public class to_do_list_item {
     private void complete_task() {
 
         //todo Database controller logic
-        if (!to_do_list_controller.update_to_do_list(toDoList)) return;
+        //if (!to_do_list_controller.update_to_do_list(toDoList)) return;
 
         task.setStrikethrough(check.isSelected());
 
