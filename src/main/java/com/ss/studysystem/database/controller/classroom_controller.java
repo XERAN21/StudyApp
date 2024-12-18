2c4ff13ebeaa3e6004ecc6027b53df2e3869bb10
XERAN <htoosetpaing1123@gmail.com>
2024-12-18 21:14:52 +0630

    ver1.0, main

--- src/main/java/com/ss/studysystem/database/controller/classroom_controller.java
+++ src/main/java/com/ss/studysystem/database/controller/classroom_controller.java
@@ -5,13 +5,10 @@ import com.ss.studysystem.Model.*;
 import com.ss.studysystem.database.connection.DB_Connection;
 
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class classroom_controller {
-    static boolean flag = true;
-
     public static boolean create_classroom(Classrooms classrooms) {
         String sql = "CALL create_classroom(?,?,?,?)";
         try (Connection connection = DB_Connection.Get_Connection();
@@ -83,6 +80,12 @@ public class classroom_controller {
             return false;
         }
     }
+
+    //todo finish this controller
+    public static boolean join_classroom(int invitation_code){
+        return true;
+    }
+
 
     /**
      * @param classroom_id
