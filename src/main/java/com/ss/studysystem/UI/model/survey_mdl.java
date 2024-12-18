4a63f22dbf0459429c3c28aba3dc750f204e3db9
nyan <ggs5times@gmail.com>
2024-12-18 09:32:22 +0630

    ver1.0

---
+++ src/main/java/com/ss/studysystem/UI/model/survey_mdl.java
@@ -0,0 +1,42 @@
+package com.ss.studysystem.UI.model;
+
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class survey_mdl {
+
+    private static survey_mdl instance;
+
+    public static survey_mdl getInstance() {
+        if (instance == null) {
+            instance = new survey_mdl();
+        }
+        return instance;
+    }
+
+    private List<Node> pages = new ArrayList<>();
+    private VBox root;
+
+    public List<Node> getPages() {
+        return pages;
+    }
+
+    public VBox getRoot() {
+        return root;
+    }
+
+    public void setRoot(VBox root) {
+        this.root = root;
+    }
+
+    public void addPage(Node n){
+        pages.add(n);
+    }
+
+    public void setPages(List<Node> pages) {
+        this.pages = pages;
+    }
+}
