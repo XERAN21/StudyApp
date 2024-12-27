e58486bb0d64709ce3d670c9acc079476334fb08
nomar <thantzin15599@gmail.com>
2024-12-27 09:04:18 +0630

    hot milfs in your area

--- src/main/java/com/ss/studysystem/UI/components/popup_builder.java
+++ src/main/java/com/ss/studysystem/UI/components/popup_builder.java
@@ -1,16 +1,16 @@
 package com.ss.studysystem.UI.components;
 
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class popup_builder {
-    public static Popup show_notification(Stage owner, Node view){
+    public static Popup show_notification(Button owner, Node view){
         Popup popup = new Popup();
         popup.setAutoHide(true);
 
         popup.getContent().add(view);
-        popup.show(owner);
         return popup;
     }
 }
