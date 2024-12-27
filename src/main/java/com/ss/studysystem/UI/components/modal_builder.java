9db4201e0be5afd0fef2aebfac0a38f697a275ba
nomar <thantzin15599@gmail.com>
2024-12-27 10:37:04 +0630

    hot milfs in your area

--- src/main/java/com/ss/studysystem/UI/components/modal_builder.java
+++ src/main/java/com/ss/studysystem/UI/components/modal_builder.java
@@ -1,6 +1,8 @@
 package com.ss.studysystem.UI.components;
 
 import com.ss.studysystem.UI.layouts.config_position;
+import com.ss.studysystem.UI.utils.config_brightness;
+import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
@@ -28,25 +30,33 @@ public class modal_builder {
     }
 
 
-    public static Stage build_fixed_modal(Stage owner, Parent view, double h, double w){
+    public static Stage build_fixed_modal(Stage owner, Parent view, double w, double h){
+
+        Platform.runLater(()->config_brightness.applyDimmingEffect(owner));
 
         Stage modalStage = new Stage();
         modalStage.initModality(Modality.APPLICATION_MODAL);
         modalStage.initOwner(owner);
         modalStage.initStyle(StageStyle.TRANSPARENT);
 
-        Scene modalScene = new Scene(view, h, w);
+        Scene modalScene = new Scene(view, w, h);
         modalStage.setScene(modalScene);
         modalScene.setFill(Color.TRANSPARENT);
 
         // modalStage.setResizable(false);
         config_position.center_stage(owner, modalStage, modalScene);
 
+        modalStage.setOnHidden(hid->Platform.runLater(()->config_brightness.removeDimmingEffect(owner)));
+
+
+
         return modalStage;
 
     }
 
     public static Stage build_fixed_modal(Stage owner, Parent view){
+
+        Platform.runLater(()->config_brightness.applyDimmingEffect(owner));
 
         Stage modalStage = new Stage();
         modalStage.initModality(Modality.APPLICATION_MODAL);
