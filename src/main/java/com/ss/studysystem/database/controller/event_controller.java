6055eb7f6cdb795c0df5300dd4e3ced5664b2a46
XERAN <htoosetpaing1123@gmail.com>
2025-01-06 02:10:23 +0630

    ver1.0, main

---
+++ src/main/java/com/ss/studysystem/database/controller/event_controller.java
@@ -0,0 +1,12 @@
+package com.ss.studysystem.database.controller;
+
+public class event_controller {
+    //todo create_event
+    //todo get_event
+    //todo get_all_event
+    //todo get_today_event
+    //todo get_week_event
+    //todo get_month_event
+    //todo update_event
+    //todo delete_event
+}
