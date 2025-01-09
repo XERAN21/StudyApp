fb5947443c468b0cae3b5e5ed52147e03b378889
nomar <thantzin15599@gmail.com>
2025-01-09 23:03:49 +0630

    she sells sea shores on the sea shells

---
+++ src/main/java/com/ss/studysystem/web/request/request_type.java
@@ -0,0 +1,12 @@
+package com.ss.studysystem.web.request;
+
+public class request_type {
+    private String action;
+    private String message;
+
+    public request_type(String action, String message) {
+        this.action = action;
+        this.message = message;
+    }
+
+}
